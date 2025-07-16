903f138102db590e93e81fd45f509a9cb6ffb414
soniaaparedees <esparedes319@gmail.com>
2025-07-15 22:28:48 -0400

    update question text and images, testing gifs

--- js/mainGame.js
+++ js/mainGame.js
@@ -1,7 +1,7 @@
 const gameData = {
     "1": {
         "text": "The TCYL Sprites and Mascots are on a mission to get to Houston to make it in time for 2025 US TCYL Humanitarian Leadership Conference.",
-        "image" : "smaller_images/beach_disaster.png",
+        "image" : "smaller_images/holding_hands.png",
 "choices": {
             "Continue": [2,[]]
         }
@@ -10,27 +10,27 @@ const gameData = {
         "text": "Okay, so I have my TC stuff, my ID, my phone, wallet, keys...hmmm am I missing anything?",
         "image" : "smaller_images/packing.png",
 "choices": {
-            "snacks": [3,["Tzu Bear"]],
-            "first aid kit": [3,["Cardi O.", "Bo D. Sattva"]],
-            "an extra shirt, can never have too little": [3,["MooMoo", "Dewy"]],
+            "Snacks": [3,["Tzu Bear"]],
+            "First aid kit": [3,["Cardi O.", "Bo D. Sattva"]],
+            "An extra shirt, can never have too little": [3,["MooMoo", "Dewy"]],
             "I'm not missing anything!": [3, ["Blaze", "Mr. Prickles"]]
         }
     },
     "3": {
-        "text": "The sprites all meet up but for some reason their flight gets cancelled.OH NO! But the conference is in two days?! What should we do?",
+        "text": "The sprites all meet up but for some reason their flight gets cancelled. OH NO! But the conference is in two days?! What should we do?",
         "image" : "smaller_images/airport.png",
 "choices": {
-            "phone a friend, ask an alum/auntie/uncle": [4, ["MooMoo", "Tzu Bear"]],
-            "cry": [4,["Mr. Prickles", "Cardi O."]],
-            "oh no, I guess I should take a nap": [4,["Dewy"]],
-            "it's time for a road trip!": [4, ["Blaze", "Bo D. Sattva"]]
+            "Phone a friend, ask an alum/auntie/uncle": [4, ["MooMoo", "Tzu Bear"]],
+            "Cry": [4,["Mr. Prickles", "Cardi O."]],
+            "Oh no, I guess I should take a nap": [4,["Dewy"]],
+            "It's time for a road trip!": [4, ["Blaze", "Bo D. Sattva"]]
         }
     },
     "4": {
         "text": "It starts to downpour and a small river forms. The sprites and mascots are all washed away!! Dewy can't swim! But there are some trees around us. How do we get to them?",
         "image" : "smaller_images/river.png",
 "choices": {
-            "parkour up the trees": [5,["Mr. Prickles"]],
+            "Parkour up the trees": [5,["Mr. Prickles"]],
             "Give everyone a jetpack that you packed in your bag.": [5,["Tzu Bear", "Cardi O."]],
             "Let's just climb and help each other up.": [5,["MooMoo", "Bo D. Sattva"]],
             "We don't need the trees, let's go down the river!": [5,[ "Blaze", "Dewy"]],
@@ -43,7 +43,7 @@ const gameData = {
             "Try to offer cover from the rain": [7,["Bo D. Sattva", "MooMoo", "Mr. Prickles"]],
             "Offer words of encouragement, to keep Blaze's inner light going": [7,["Blaze"]],
             "Ask kindly if the bird can fly just a bit faster!": [7,["Dewy", "Tzu Bear"]],
-            "Starts flapping arms to help the bird fly faster?": [7,["Cardi O."]],
+            "Start flapping arms to help the bird fly faster?": [7,["Cardi O."]],
         }
     },
     "7": {
@@ -59,40 +59,53 @@ const gameData = {
 "choices": {
             "Tell them we can grow by facing our fears": [9,["MooMoo", "Blaze"]],
             "Suggest we can find another route": [9,["Dewy"]],
-            "Asks Spike why they are afraid of balloons": [9, ["Bo D. Sattva"]],
-            "Ask if Spike would like a hug (haha ouch)": [9, ["Cardi O.","Mr. Prickles","Tzu Bear"]],
+            "Ask Spike why they are afraid of balloons": [9, ["Bo D. Sattva"]],
+            "Ask if Spike would like a hug": [9, ["Cardi O.","Mr. Prickles","Tzu Bear"]],
         }
     },
     "9": {
         "text": "The party comes across a festival. \
-                There's so much yummy vegetarian food and toys and games! \
-                Suddenly, the squad comes across a lost, crying dog.\
-                HOW SHALL WE PROCEED? WHAT IS THE DEED?",
+                There's so much yummy vegetarian food, toys, and games! \
+                Suddenly, the squad comes across a lost, crying dog.",
         "image" : "smaller_images/festival.png",
 "choices": {
-            "Offer some of your food to the dog": [10, ["Tzu Bear", "Cardi O."]],
-            "Distracts the dog by making goofy faces to stop it from crying": [10, ["Blaze", "Mr. Prickles"]],
-            "Contact the festival runners to let them know someone's dog is missing": [10, ["MooMoo"]],
-            "Speak gently with kind words to the dog to calm them down.": [10, ["Bo"]],
+            "Continue": [10,[]]
         }
     },
-    "10" :{
-        "text": "The dog then magically starts speaking. Turns out it was a test! AND YOU PASSED.",
-        "image" : "smaller_images/window_jump.png",
+    "10": {
+        "text": "HOW SHALL WE PROCEED? WHAT IS THE DEED?",
+        "image" : "smaller_images/eyebrows-wink.gif",
 "choices": {
-            "Continue": [11,[]]
+            "Offer some of your food to the dog": [11, ["Tzu Bear", "Cardi O."]],
+            "Distract the dog by making goofy faces to stop it from crying": [11, ["Blaze", "Mr. Prickles"]],
+            "Contact the festival runners to let them know someone's dog is missing": [11, ["MooMoo"]],
+            "Speak gently with kind words to the dog to calm them down.": [11, ["Bo"]],
         }
     },
     "11" :{
+        "text": "The dog then magically starts speaking. Turns out it was a test! AND YOU PASSED.",
+        "image" : "smaller_images/eyebrows-wink.gif",
+"choices": {
+            "Continue": [12,[]]
+        }
+    },
+    "12" :{
         "text" : "By the magical power of the dog you and the squad are now transported to Houston. \
                   How do you celebrate?",
-        "image" : "smaller_images/grocery_bag.png",
+        "image" : "smaller_images/holding_hands.png",
 "choices": {
-                    "food": [0, ["Tzu Bear"]],
-                    "A k-pop dance cover performance": [0, ["Blaze", "Mr. Prickles","Cardi O."]],
-                    "A soulful performance of TC songs with sign language": [0, ["Bo D. Sattva"]],
-                    "A nice nap.": [0, ["Dewy","MooMoo"]],
-                }
+            "food": [13, ["Tzu Bear"]],
+            "A k-pop dance cover performance": [13, ["Blaze", "Mr. Prickles","Cardi O."]],
+            "A soulful performance of TC songs with sign language": [13, ["Bo D. Sattva"]],
+            "A nice nap.": [13, ["Dewy","MooMoo"]],
+        }
+    },
+    "13" :{
+        "text" : "Loading your character ...",
+        "image" : "smaller_images/loading.gif",
+"choices": {
+            "Reveal character": [0,[]]
+        }
     }
 
 };
@@ -178,7 +191,7 @@ function revealMostSelectedVegetable() {
         storyImage.style.display = 'none';
         choicesContainer.style.display = 'none';
     
-        text.textContent = "Drumroll... you are (Right click or hold the image to save)";
+        text.textContent = "(Right click or hold the image to save)       You are ..... ";
         text.appendChild(img);
 
         // Share button functionality
